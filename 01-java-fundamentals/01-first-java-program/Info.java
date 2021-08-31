1afee1a83bb0a42c1450e0c80c3d346105933ea1
Nathan Langione <nlangione@wayfair.com>
2021-08-31 11:15:49 -0400

    first java program assignment

---
+++ 01-java-fundamentals/01-first-java-program/Info.java
@@ -0,0 +1,7 @@
+public class Info{
+    public static void main(String[] args){
+        System.out.println("My name is Nathan Langione");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Keene, NH");
+    }
+}
