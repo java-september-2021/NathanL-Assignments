56f4b75ad58a48b22bf279f993135b3b2c1b5586
Nathan Langione <nlangione@wayfair.com>
2021-09-02 19:14:23 -0400

    starting fizzbuzz

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTester.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTester {
+    public void count(){
+        for(int i = 1; i <=100; i++){
+            String result = fizzbuzz(i);
+            System.out.printf("i: ")
+        }
+    }
+}
