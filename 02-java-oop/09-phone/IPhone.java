6c9f249cf46048bd09e9e32b9a00acfd8bda27fe
Nathan Langione <nlangione@wayfair.com>
2021-09-10 10:56:52 -0400

    Staring bank account assignment

---
+++ 02-java-oop/09-phone/IPhone.java
@@ -0,0 +1,20 @@
+public class IPhone extends Phone implements Ringable {
+    public IPhone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+
+    @Override
+    public String ring() {
+        // your code here
+    }
+
+    @Override
+    public String unlock() {
+        // your code here
+    }
+
+    @Override
+    public void displayInfo() {
+        // your code here
+    }
+}
---
+++ 02-java-oop/09-phone/Phone.java
@@ -0,0 +1,17 @@
+public abstract class Phone {
+    private String versionNumber;
+    private int batteryPercentage;
+    private String carrier;
+    private String ringTone;
+
+    public Phone(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        this.versionNumber = versionNumber;
+        this.batteryPercentage = batteryPercentage;
+        this.carrier = carrier;
+        this.ringTone = ringTone;
+    }
+
+    // abstract method. This method will be implemented by the subclasses
+    public abstract void displayInfo();
+    // getters and setters removed for brevity. Please implement them yourself
+}
