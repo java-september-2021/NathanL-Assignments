627a30467ce0fb02123e0185d324bdb09cea7711
Nathan Langione <nlangione@wayfair.com>
2021-09-17 09:41:04 -0400

    Completed assignemnt

--- 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/Projectone/src/main/java/com/nlangione/projectone/DojoController/DojoController.java
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/Projectone/src/main/java/com/nlangione/projectone/DojoController/DojoController.java
@@ -6,20 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DojoController {
-	@RequestMapping("/{dojo}")
-	public String index(@PathVariable("dojo") String foo) {
+	@RequestMapping("/dojo")
+	public String dojo() {
 		System.out.println("The dojo is awesome!");
 		return "The dojo is awesome!";
 	}
 	
-	@RequestMapping("/dojo/{burbank}")
-	public String burbank(@PathVariable("burbank") String foo) {
-		System.out.println("Burbank Dojo is located in Southern California");
-		return "Burbank Dojo is located in Southern California";
-	}
-	
-	@RequestMapping("/dojo/{part1}-{part2}")
-	public String sj(@PathVariable("part1") String foo,@PathVariable("part2") String bar) {
-	return "SJ dojo is the headquarters";
+	@RequestMapping("/dojo/{location}")
+	public String dojoCenter(@PathVariable("location") String location) {
+		switch(location) {
+		case "burbank":
+			System.out.println("Burbank Dojo is located in Southern California");
+			return "Burbank Dojo is located in Southern California";
+		case "san-jose":
+			System.out.println("SJ dojo is the headquarters");
+			return "SJ dojo is the headquarters";
+		default:
+			System.out.printf("%s is pretty sweet!", location);
+			return String.format("%s is pretty sweet!", location);
+		}
 	}
 }
