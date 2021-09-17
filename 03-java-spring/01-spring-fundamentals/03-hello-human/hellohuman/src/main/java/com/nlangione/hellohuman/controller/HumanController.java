66a95f8eb0886321e6dfd91bcbd89b779863bff1
Nathan Langione <nlangione@wayfair.com>
2021-09-17 11:37:37 -0400

    fixed config to display page

--- 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/nlangione/hellohuman/controller/HumanController.java
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/nlangione/hellohuman/controller/HumanController.java
@@ -1,22 +1,15 @@
 package com.nlangione.hellohuman.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HumanController {
-
-@RequestMapping("/")
-public String index(@RequestParam(value="q", required=false) String userName, Model model) {
-	System.out.println(userName);
-	if(userName == null) {
-		return "Hello Human!";
-	} else {
-		return "hello " + userName;
+	@RequestMapping("/")
+	public String index(Model model, @RequestParam(value="name", required=false, defaultValue="Human") String name) {
+		model.addAttribute("name", name);
+		return "index.jsp";
 	}
-	
-	
-}
 }
--- 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/webapp/WEB-INF/index.jsp
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/webapp/WEB-INF/index.jsp
@@ -1,15 +1,13 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset="ISO-8859-1">
 <title>Hello Human</title>
-<link rel="Stlesheet" href="/css/style.css">
 </head>
 <body>
-<h1> Hello Human!</h1>
-<h3>Welcome to SpringBoot!</h3>
+	<h1>Hello ${ name }</h1>
+	<p>Welcome to SpringBoot!</p>
 </body>
 </html>
-
