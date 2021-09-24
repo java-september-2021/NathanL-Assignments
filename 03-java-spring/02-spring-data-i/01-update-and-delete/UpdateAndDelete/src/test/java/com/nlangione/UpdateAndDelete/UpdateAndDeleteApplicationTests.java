c7eaecb154116a46292337bfeddc91fb1ca1b1e9
Nathan Langione <nlangione@wayfair.com>
2021-09-24 09:36:00 -0400

    Completed assignment

---
+++ 03-java-spring/02-spring-data-i/01-update-and-delete/UpdateAndDelete/src/test/java/com/nlangione/UpdateAndDelete/UpdateAndDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nlangione.UpdateAndDelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateAndDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
