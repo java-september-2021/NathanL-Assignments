ee18e7bd4c2341805d3aff310ad882aaed690f0f
Nathan Langione <nlangione@wayfair.com>
2021-10-15 11:02:10 -0400

    Added @ to repo

--- 03-java-spring/03-spring-data-ii/05-products-and-categories/ProductsAndCategories/src/main/java/com/nlangione/ProductsAndCategories/repositories/ProductRepository.java
+++ 03-java-spring/03-spring-data-ii/05-products-and-categories/ProductsAndCategories/src/main/java/com/nlangione/ProductsAndCategories/repositories/ProductRepository.java
@@ -3,10 +3,12 @@ package com.nlangione.ProductsAndCategories.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nlangione.ProductsAndCategories.models.Category;
 import com.nlangione.ProductsAndCategories.models.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 	// this method retrieves all the Products from the database
 	List<Product> findAll();
